cc63f8b332428d94c717f410927036e42b8fde1e
Steve Mostovoy <stevemostovoysm@gmail.com>
2024-07-20 14:11:11 -0700

    Properly set up poetry & ruff.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 import sys
+
 import ecosystem
 
 if __name__ == "__main__":
-
     if len(sys.argv) > 1 and sys.argv[1] == "--gifs":
         print("Running in GIF generation mode...")
         for gif_path, timestamp in ecosystem.run_gif_generator(duration=180):
