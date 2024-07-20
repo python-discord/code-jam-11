ed2541bd1d2f4e1a4343b86d7101469e6e118eca
Paillat <me@paillat.dev>
2024-07-20 15:06:59 +0200

    :sparkles: Add base  tvdb client (incomplete)

--- src/tvdb/client.py
+++ src/tvdb/client.py
@@ -1,14 +1,89 @@
-from typing import ClassVar, Literal, overload
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar, Literal, overload, override
 
 import aiohttp
 from yarl import URL
 
 from src.settings import TVDB_API_KEY
+from src.tvdb.generated_models import SearchResult, SeriesBaseRecord
+from src.tvdb.models import SearchResponse, SeriesResponse
 from src.utils.log import get_logger
 
 log = get_logger(__name__)
 
-type JSON_DATA = dict[str, JSON_DATA] | list[JSON_DATA] | str | int | float | bool | None
+type JSON_DATA = dict[str, JSON_DATA] | list[JSON_DATA] | str | int | float | bool | None  # noice
+
+
+class _Media(ABC):
+    def __init__(self, client: "TvdbClient"):
+        self.client = client
+        self._id: int | None = None
+
+    @property
+    def id(self) -> int | str | None:
+        return self._id
+
+    @id.setter
+    def id(self, value: int | str) -> None:
+        self._id = int(str(value).split("-")[1])
+
+    def __call__(self, media_id: str) -> "_Media":
+        self.id = media_id
+        return self
+
+    @abstractmethod
+    async def search(self, search_query: str, limit: int = 1) -> list[Any]: ...
+
+    @abstractmethod
+    async def fetch(self, *, extended: bool = False) -> Any: ...
+
+
+class Series(_Media):
+    """Class to interact with the TVDB API for series."""
+
+    @override
+    async def search(self, search_query: str, limit: int = 1) -> list[SearchResult]:
+        """Search for a series in the TVDB database.
+
+        :param search_query:
+        :param limit:
+        :return:
+        """
+        return await self.client.search(search_query, "series", limit)
+
+    @override
+    async def fetch(self, *, extended: bool = False) -> SeriesBaseRecord:
+        """Fetch a series by its ID.
+
+        :param extended:
+        :return:
+        """
+        data = await self.client.request("GET", f"series/{self.id}" + ("/extended" if extended else ""))
+        return SeriesResponse(**data).data  # pyright: ignore[reportCallIssue]
+
+
+class Movies(_Media):
+    """Class to interact with the TVDB API for movies."""
+
+    @override
+    async def search(self, search_query: str, limit: int = 1) -> list[SearchResult]:
+        """Search for a movie in the TVDB database.
+
+        :param search_query:
+        :param limit:
+        :return: list[SearchResult]
+        """
+        return await self.client.search(search_query, "movie", limit)
+
+    @override
+    async def fetch(self, *, extended: bool = False) -> SeriesBaseRecord:
+        """Fetch a movie by its ID.
+
+        :param extended:  Whether to fetch extended information.
+        :return:
+        """
+        data = await self.client.request("GET", f"movies/{self.id}" + ("/extended" if extended else ""))
+        return SeriesResponse(**data).data  # pyright: ignore[reportCallIssue]
 
 
 class InvalidApiKeyError(Exception):
@@ -28,31 +103,60 @@ class TvdbClient:
     def __init__(self, http_session: aiohttp.ClientSession):
         self.http_session = http_session
         self.auth_token = None
+        self.series = Series(self)
+        self.movies = Movies(self)
 
     @overload
-    async def request(self, method: Literal["GET"], endpoint: str, body: None = None) -> JSON_DATA: ...
+    async def request(
+        self,
+        method: Literal["GET"],
+        endpoint: str,
+        body: None = None,
+        query: dict[str, str] | None = None,
+    ) -> JSON_DATA: ...
 
     @overload
-    async def request(self, method: Literal["POST"], endpoint: str, body: JSON_DATA) -> JSON_DATA: ...
+    async def request(
+        self, method: Literal["POST"], endpoint: str, body: JSON_DATA, query: None = None
+    ) -> JSON_DATA: ...
 
-    async def request(self, method: Literal["GET", "POST"], endpoint: str, body: JSON_DATA = None) -> JSON_DATA:
+    async def request(
+        self,
+        method: Literal["GET", "POST"],
+        endpoint: str,
+        body: JSON_DATA = None,
+        query: dict[str, str] | None = None,
+    ) -> JSON_DATA:
         """Make an authorized request to the TVDB API."""
         log.trace(f"Making TVDB {method} request to {endpoint}")
 
         if self.auth_token is None:
             log.trace("No auth token found, requesting initial login.")
             await self._login()
-        headers = {"Authorization": f"Bearer {self.auth_token}"}
+        headers = {"Authorization": f"Bearer {self.auth_token}", "Accept": "application/json"}
 
         url = self.BASE_URL / endpoint.removeprefix("/")
+        if method == "GET" and query:
+            url = url.with_query(query)
         async with self.http_session.request(method, url, headers=headers, json=body) as response:
             if response.status == 401:
                 log.debug("TVDB API token expired, requesting new token.")
                 self.auth_token = None
+                # TODO: might be an infinite loop
                 return await self.request(method, endpoint, body)  # pyright: ignore[reportCallIssue,reportArgumentType]
-
             response.raise_for_status()
             return await response.json()
+
+    async def search(
+        self, search_query: str, entity_type: Literal["series", "movie", "all"] = "all", limit: int = 1
+    ) -> list[SearchResult]:
+        """Search for a series or movie in the TVDB database."""
+        query: dict[str, str] = {"query": search_query, "limit": str(limit)}
+        if entity_type != "all":
+            query["type"] = entity_type
+        data = await self.request("GET", "search", query=query)
+        response = SearchResponse(**data)  # pyright: ignore[reportCallIssue]
+        return response.data
 
     async def _login(self) -> None:
         """Obtain the auth token from the TVDB API.
--- src/tvdb/models.py
+++ src/tvdb/models.py
@@ -1,66 +1,34 @@
 from pydantic import BaseModel
 
-
-class _RemoteID(BaseModel):
-    id: str
-    type: int
-    sourceName: str  # noqa: N815
+from src.tvdb.generated_models import Links, SearchResult, SeriesBaseRecord, SeriesExtendedRecord
 
 
-class _Translation(BaseModel):
-    ara: str | None = None
-    ces: str | None = None
-    deu: str | None = None
+class Response(BaseModel):
+    """Model for any response of the TVDB API."""
 
-
-class SearchData(BaseModel):
-    """Model for the data objects returned by the TVDB search endpoint."""
-
-    aliases: list[str]
-    companies: list[str] | None = None
-    companyType: str | None = None  # noqa: N815
-    country: str
-    director: str | None = None
-    first_air_time: str
-    genres: list[str] | None = None
-    id: str
-    image_url: str
-    name: str
-    is_official: bool | None = None
-    name_translated: str | None = None
-    network: str
-    objectID: str  # noqa: N815
-    officialList: str | None = None  # noqa: N815
-    overview: str
-    overviews: _Translation
-    overview_translated: list[str] | None = None
-    poster: str | None = None
-    posters: list[str] | None = None
-    primary_language: str
-    remote_ids: list[_RemoteID]
     status: str
-    slug: str
-    studios: list[str] | None = None
-    title: str | None = None
-    thumbnail: str
-    translations: _Translation
-    translationsWithLang: list[str] | None = None  # noqa: N815
-    tvdb_id: str
-    type: str
-    year: str
 
 
-class _Links(BaseModel):
-    prev: str | None = None  # none if on the first page
-    self: str
-    next: str
-    total_items: int
-    page_size: int
+class PaginatedResponse(Response):
+    """Model for the response of the search endpoint of the TVDB API."""
+
+    links: Links
 
 
-class SearchResponse(BaseModel):
-    """Model for the response from the TVDB search endpoint."""
+class SearchResponse(PaginatedResponse):
+    """Model for the response of the search endpoint of the TVDB API."""
 
-    data: list[SearchData]
-    status: str
-    links: _Links
+    data: list[SearchResult]
+
+
+class SeriesResponse(Response):
+    """Model for the response of the series/{id} endpoint of the TVDB API."""
+
+    data: SeriesBaseRecord
+
+
+class SeriesExtendedResponse(Response):
+    """Model for the response of the series/{id}/extended endpoint of the TVDB API."""
+
+    data: SeriesExtendedRecord
+    episodes: list[dict[str, str]]
