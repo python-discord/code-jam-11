31ea0a41561ef563eb37d9cd43a73e6dfc84aa9a
ItsDrike <itsdrike@protonmail.com>
2024-07-20 11:05:50 +0200

    Remove unused bot parameter from mention_command func
    This parameter was left there from when the function still only accepted
    strings for the command name and performed a search for the command
    object using the bot instance.

--- src/exts/help/help.py
+++ src/exts/help/help.py
@@ -29,14 +29,14 @@ class HelpCog(Cog):
                 continue
 
             if isinstance(command, SlashCommand):
-                embed.add_field(name=mention_command(command, self.bot), value=command.description, inline=False)
+                embed.add_field(name=mention_command(command), value=command.description, inline=False)
             if isinstance(command, SlashCommandGroup):
                 embed.add_field(
-                    name=f"{mention_command(command, self.bot)} group",
+                    name=f"{mention_command(command)} group",
                     value=command.description
                     + "\n\n"
                     + "\n".join(
-                        f"{mention_command(subcommand, self.bot)}: {subcommand.description}"
+                        f"{mention_command(subcommand)}: {subcommand.description}"
                         for subcommand in command.subcommands
                     ),
                     inline=False,
--- src/utils/markdown.py
+++ src/utils/markdown.py
@@ -3,6 +3,6 @@ from typing import Any
 import discord
 
 
-def mention_command(command: discord.ApplicationCommand[Any, ..., Any], bot: discord.Bot) -> str:
+def mention_command(command: discord.ApplicationCommand[Any, ..., Any]) -> str:
     """Mentions the command using discord markdown."""
     return f"</{command.qualified_name}:{command.qualified_id}>"
