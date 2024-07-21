8c97bf284951ffb1335b368873749d0bd3c7c99c
ItsDrike <itsdrike@protonmail.com>
2024-07-21 18:59:49 +0200

    Run ruff as post-hook for alembic autogen migrations

--- alembic-migrations/versions/2024_07_21_1854-c55da3c62644_initial_migration.py
+++ alembic-migrations/versions/2024_07_21_1854-c55da3c62644_initial_migration.py
@@ -1,17 +1,14 @@
 """Initial migration
 
 Revision ID: c55da3c62644
-Revises: 
+Revises:
 Create Date: 2024-07-21 18:54:26.159716
 """
 
 from collections.abc import Sequence
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
-revision: str = 'c55da3c62644'
+revision: str = "c55da3c62644"
 down_revision: str | None = None
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
