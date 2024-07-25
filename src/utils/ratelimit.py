3b61f4c620f8d3e4fdb8e9378c14b8e3a067b6eb
ItsDrike <itsdrike@protonmail.com>
2024-07-25 14:46:35 +0200

    Add rate limit decorator function

--- src/utils/ratelimit.py
+++ src/utils/ratelimit.py
@@ -1,8 +1,12 @@
 import time
-from typing import cast
+from collections.abc import Awaitable, Callable
+from functools import wraps
+from typing import Concatenate, cast
 
 from aiocache import BaseCache
+from discord import ApplicationContext, Cog
 
+from src.bot import Bot
 from src.utils.log import get_logger
 
 log = get_logger(__name__)
@@ -134,3 +138,72 @@ async def rate_limit(
             closest_expiration=closest_expiration,
             updates_when_exceeded=update_when_exceeded,
         )
+
+
+type CogCommandFunction[T: Cog, **P] = Callable[Concatenate[T, ApplicationContext, P], Awaitable[None]]
+type TransformFunction[T, R] = Callable[[T, ApplicationContext], R]
+
+
+def rate_limited[T: Cog, **P](
+    key: str | Callable[[T, ApplicationContext], str],
+    *,
+    limit: int | TransformFunction[T, int],
+    period: float | TransformFunction[T, float],
+    update_when_exceeded: bool | TransformFunction[T, bool] = False,
+    err_msg: str | None | TransformFunction[T, str | None] = None,
+) -> Callable[[CogCommandFunction[T, P]], CogCommandFunction[T, P]]:
+    """Apply rate limits to given cog command function.
+
+    The decorated function must be a slash command function that belongs to a cog class
+    (as an instance method). Make sure to apply this decorator before the ``slash_command``
+    decorator.
+
+    This uses the :func:`rate_limit` function internally to enforce the rate limits.
+    See its description for more info.
+
+    All of the parameters can be set directly, or they can be callables, which will get called
+    with self (the cog instance) and ctx, using the return value as the value of that parameter.
+    These parameters will then all be forwarded to the ``rate_limit`` function.
+
+    .. note::
+        Py-cord does provide a built-in way to rate-limit commands through "cooldown" structures.
+        These work similarly to our custom implementation, but bucketing isn't as flexible and
+        doesn't work globally across the whole application.
+
+        Using this decorator is therefore preferred, even for simple rate limits, for consistency.
+    """
+
+    def inner(func: CogCommandFunction[T, P]) -> CogCommandFunction[T, P]:
+        @wraps(func)
+        async def wrapper(self: T, ctx: ApplicationContext, *args: P.args, **kwargs: P.kwargs) -> None:
+            bot = ctx.bot
+            if not isinstance(bot, Bot):
+                raise TypeError(
+                    "The bot instance must be of our custom Bot type (src.bot.Bot), "
+                    f"found: {bot.__class__.__qualname__}"
+                )
+
+            cache = bot.cache
+
+            # Call transformer functions, if used
+            key_ = key(self, ctx) if isinstance(key, Callable) else key
+            limit_ = limit(self, ctx) if isinstance(limit, Callable) else limit
+            period_ = period(self, ctx) if isinstance(period, Callable) else period
+            update_when_exceeded_ = (
+                update_when_exceeded(self, ctx) if isinstance(update_when_exceeded, Callable) else update_when_exceeded
+            )
+            err_msg_ = err_msg(self, ctx) if isinstance(err_msg, Callable) else err_msg
+
+            await rate_limit(
+                cache,
+                key_,
+                limit=limit_,
+                period=period_,
+                update_when_exceeded=update_when_exceeded_,
+                err_msg=err_msg_,
+            )
+            return await func(self, ctx, *args, **kwargs)
+
+        return wrapper
+
+    return inner
